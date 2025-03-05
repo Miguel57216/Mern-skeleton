57dbabb8157291bacc96bd4af6dd718bab567b58
Miguel57216 <vegadiazmiguelangel94@gmail.com>
2025-03-04 20:39:11 -0500

    Update app.js & MainRouter.js

--- client/App.js
+++ client/App.js
@@ -8,15 +8,15 @@ import { hot } from 'react-hot-loader'
 const App = () => {
   React.useEffect(() => {
     const jssStyles = document.querySelector('#jss-server-side')
-    if (jssStyles) {
+    if (jssStyles) {            
       jssStyles.parentNode.removeChild(jssStyles)
     }
   }, [])
   return (
   <BrowserRouter>
       <ThemeProvider theme={theme}>
-        <MainRouter/>
-      </ThemeProvider>
+        <MainRouter/>              
+      </ThemeProvider>          
   </BrowserRouter>
 )}
 
--- client/MainRouter.js
+++ client/MainRouter.js
@@ -12,14 +12,14 @@ import Menu from './core/Menu'
 const MainRouter = () => {
     return (<div>
       <Menu/>
-      <Switch>
+      <Switch>          
         <Route exact path="/" component={Home}/>
-        <Route path="/users" component={Users}/>
+        <Route path="/users" component={Users}/>            
         <Route path="/signup" component={Signup}/>
-        <Route path="/signin" component={Signin}/>
+        <Route path="/signin" component={Signin}/>           
         <PrivateRoute path="/user/edit/:userId" component={EditProfile}/>
-        <Route path="/user/:userId" component={Profile}/>
-      </Switch>
+        <Route path="/user/:userId" component={Profile}/>                      
+      </Switch>                             
     </div>)
 }
 
